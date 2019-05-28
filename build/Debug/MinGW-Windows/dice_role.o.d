0c5330fb1ca187870d09b6357a66adc6c8c926b7
neeraj kaberpanthi <neeraj kaberpanthi@SEGULA-11>
2019-05-28 18:41:07 +0530

    Dice_role added

---
+++ build/Debug/MinGW-Windows/dice_role.o.d
@@ -0,0 +1,1 @@
+build/Debug/MinGW-Windows/dice_role.o: dice_role.cpp
--- welcome.cc
+++ welcome.cc
@@ -30,17 +30,17 @@
 
 #include <iostream>
 
-int main(int argc, char**argv) {
-    // Prints welcome message...
-    std::cout << "Welcome ..." << std::endl;
-
-    // Prints arguments...
-    if (argc > 1) {
-        std::cout << std::endl << "Arguments:" << std::endl;
-        for (int i = 1; i < argc; i++) {
-            std::cout << i << ": " << argv[i] << std::endl;
-        }
-    }
-    
-    return 0;
-}
+//int main(int argc, char**argv) {
+//    // Prints welcome message...
+//    std::cout << "Welcome ..." << std::endl;
+//
+//    // Prints arguments...
+//    if (argc > 1) {
+//        std::cout << std::endl << "Arguments:" << std::endl;
+//        for (int i = 1; i < argc; i++) {
+//            std::cout << i << ": " << argv[i] << std::endl;
+//        }
+//    }
+//    
+//    return 0;
+//}
